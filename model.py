270fcf3485a46b42222a44031f5e36b29e32504e
Shengfeng Li <cs.shengfeng.li@gmail.com>
2018-04-27 12:27:40 -0500

    Load if trained

--- model.py
+++ model.py
@@ -5,38 +5,53 @@ from keras.layers import Conv2D, MaxPooling2D, Dense, \
 from keras.models import Sequential, load_model
 
 saved_dir = './saved'
-model_path = 'best_model.h5'
-model_history_path = 'best_model_history'
+# model_path = 'best_model.h5'
+# model_history_path = 'best_model_history'
+
+_num_classes = 0
+_input_shape = None
 
 
-def get_model(num_classes, input_shape):
-    # Load saved model if exists
+def set_model_params(num_classes, input_shape):
+    global _num_classes
+    global _input_shape
+    _num_classes = num_classes
+    _input_shape = input_shape
+
+
+def create_model():
+    print("-------------------Create model---------------------------")
+    model = Sequential()
+    model.add(Conv2D(64, (3, 3), activation='relu', padding='same',
+                     input_shape=_input_shape))
+    model.add(Conv2D(64, (3, 3), activation='relu'))
+    model.add(MaxPooling2D(pool_size=(2, 2)))
+    model.add(Dropout(0.25))
+
+    model.add(Conv2D(128, (3, 3), activation='relu', padding='same'))
+    model.add(Conv2D(128, (3, 3), activation='relu'))
+    model.add(MaxPooling2D(pool_size=(2, 2)))
+    model.add(Dropout(0.25))
+
+    model.add(Conv2D(256, (3, 3), activation='relu', padding='same'))
+    model.add(Conv2D(256, (3, 3), activation='relu'))
+    model.add(MaxPooling2D(pool_size=(2, 2)))
+    model.add(Dropout(0.5))
+
+    model.add(Flatten())
+
+    model.add(Dense(512, activation='relu'))
+
+    model.add(Dense(_num_classes, activation='softmax'))
+    model.summary()
+    return model
+
+
+def load_saved_model(model_path):
+    model = None
     if os.path.exists(model_path):
+        # Load saved model if exists
         print("-------------------Load model---------------------------")
         model = load_model(model_path)
-    else:
-        print("-------------------Create model---------------------------")
-        model = Sequential()
-        model.add(Conv2D(64, (3, 3), activation='relu', padding='same',
-                         input_shape=input_shape))
-        model.add(Conv2D(64, (3, 3), activation='relu'))
-        model.add(MaxPooling2D(pool_size=(2, 2)))
-        model.add(Dropout(0.25))
-
-        model.add(Conv2D(128, (3, 3), activation='relu', padding='same'))
-        model.add(Conv2D(128, (3, 3), activation='relu'))
-        model.add(MaxPooling2D(pool_size=(2, 2)))
-        model.add(Dropout(0.25))
-
-        model.add(Conv2D(256, (3, 3), activation='relu', padding='same'))
-        model.add(Conv2D(256, (3, 3), activation='relu'))
-        model.add(MaxPooling2D(pool_size=(2, 2)))
-        model.add(Dropout(0.5))
-
-        model.add(Flatten())
-
-        model.add(Dense(512, activation='relu'))
-
-        model.add(Dense(num_classes, activation='softmax'))
-    model.summary()
+        model.summary()
     return model
--- train.py
+++ train.py
@@ -9,7 +9,7 @@ from keras.models import clone_model
 from keras.optimizers import Adam
 from sklearn.metrics import confusion_matrix
 
-from model import model_path, saved_dir, model_history_path, get_model
+from model import set_model_params, saved_dir, create_model, load_saved_model
 
 (x_train, y_train), (x_test, y_test) = cifar10.load_data()
 
@@ -24,35 +24,52 @@ print("Testing set size:\t", len(y_test))
 class_names = ['airplane', 'automobile', 'bird', 'cat', 'deer', 'dog', 'frog',
                'horse', 'ship', 'truck']
 
+DEFAULT_MODEL_PATH = 'best_model.h5'
+DEFAULT_MODEL_HISTORY_PATH = 'best_model_history'
+DEF_TRAN_PATH = 'transition_model.h5'
+DEF_TRAN_HIS_PATH = 'transition_model_history'
 
-def train(_model, saved_path, epochs, patience):
-    _model.compile(loss='sparse_categorical_crossentropy',
-                   optimizer=Adam(lr=1.0e-4),
-                   # Adam optimizer with 1.0e-4 learning rate
-                   # Metrics to be evaluated by the model
-                   metrics=['accuracy'])
+DEF_FINAL_PATH = 'final_model.h5'
 
-    checkpoint = ModelCheckpoint(saved_dir + '/' + saved_path,
-                                 # model filename
-                                 monitor='val_loss',  # quantity to monitor
-                                 verbose=0,  # verbosity - 0 or 1
-                                 save_best_only=True,
-                                 # The latest best model will not be
-                                 # overwritten
-                                 # The decision to overwrite model
-                                 mode='auto')
+set_model_params(num_classes, (IMAGE_SIZE, IMAGE_SIZE, CHANNELS))
+
+
+def train(_model, epochs, patience, saved_path=None):
+    _model = compile_model(_model)
+    cb_list = []
+    if saved_path is not None:
+        checkpoint = ModelCheckpoint(saved_dir + '/' + saved_path,
+                                     # model filename
+                                     monitor='val_loss',  # quantity to monitor
+                                     verbose=0,  # verbosity - 0 or 1
+                                     save_best_only=True,
+                                     # The latest best model will not be
+                                     # overwritten
+                                     # The decision to overwrite model
+                                     mode='auto')
+        cb_list.append(checkpoint)
 
     early_stopping = EarlyStopping(monitor="val_loss", patience=patience,
                                    verbose=1, mode='auto')
+    cb_list.append(early_stopping)
 
     _model_details = _model.fit(x_train, y_train,
                                 batch_size=32,
                                 # number of samples per gradient update
                                 epochs=epochs,  # number of iterations
                                 validation_data=(x_test, y_test),
-                                callbacks=[checkpoint, early_stopping],
+                                callbacks=cb_list,
                                 verbose=2)
     return _model, _model_details
+
+
+def compile_model(_model):
+    _model.compile(loss='sparse_categorical_crossentropy',
+                   optimizer=Adam(lr=1.0e-4),
+                   # Adam optimizer with 1.0e-4 learning rate
+                   # Metrics to be evaluated by the model
+                   metrics=['accuracy'])
+    return _model
 
 
 if not os.path.exists(saved_dir):
@@ -65,12 +82,14 @@ def save_history(_model_details, _history_path):
         pickle.dump(_model_details.history, file_pi)
 
 
-model = get_model(num_classes, (IMAGE_SIZE, IMAGE_SIZE, CHANNELS))
+model = load_saved_model(DEFAULT_MODEL_PATH)
+if model is None:
+    model = create_model()
 
 # Train default model
-model, model_details = train(model, model_path, 100, 10)
+model, model_details = train(model, 100, 10, saved_path=DEFAULT_MODEL_PATH,)
 
-save_history(model_details, model_history_path)
+save_history(model_details, DEFAULT_MODEL_HISTORY_PATH)
 
 # Evaluate the model
 score_ori = model.evaluate(x_test, y_test, verbose=0)
@@ -135,15 +154,20 @@ for layer_name, thresholds in zip(prune_layouts, prune_thresholds):
         # weights[0] is weight tensor, weights[1] is bias tensor,
         # prune weights only
         print('Current threshold is {:.3f}'.format(threshold))
-        weights_copy = np.copy(weights)
-        weights_copy[0] = prune(weights_copy[0], threshold)
-        layer.set_weights(weights_copy)
-        print("sparsity:",
-              np.count_nonzero(weights_copy[0]) / weights_copy[0].size, '\n')
         saved_name = layer_name + '_{:.3f}'.format(threshold)
-        model_copy, model_copy_detail = train(model_copy, saved_name + '.h5',
-                                              50, 5)
-        save_history(model_copy_detail, saved_name + '_history')
+        tmp_model = load_saved_model(saved_name + '.h5')
+        if tmp_model is not None:
+            print('Already trained')
+            model_copy = compile_model(tmp_model)
+        else:
+            weights_copy = np.copy(weights)
+            weights_copy[0] = prune(weights_copy[0], threshold)
+            layer.set_weights(weights_copy)
+            print("sparsity:",
+                  np.count_nonzero(weights_copy[0]) / weights_copy[0].size, '\n')
+            model_copy, model_copy_detail = train(model_copy, 50, 5)
+            model_copy.save(filepath=saved_name + '.h5')
+            save_history(model_copy_detail, saved_name + '_history')
         score_copy = model_copy.evaluate(x_test, y_test, verbose=0)
         print("After Pruning, accuracy: %.2f%%" % (score_copy[1] * 100) + '\n')
         accuracy_threshold.append((threshold, score_copy[1]))
@@ -158,57 +182,61 @@ for k in optimal_sol:
     print(k, ':', optimal_sol[k])
 
 print("\n---------------Combine Pruning Training-----------------------------")
-model_transition = clone_model(model)
-model_transition.set_weights(model.get_weights())
-# Generate layer dictionary from cloned model
-layer_dict = dict([(layer.name, layer) for layer in model_transition.layers])
-for layer_name in prune_layouts:
-    t_layer = layer_dict[layer_name]
-    t_weights = t_layer.get_weights()
-    t_threshold = optimal_sol[layer_name]
-    t_weights[0] = prune(t_weights[0], t_threshold)
-    t_layer.set_weights(t_weights)
-    print("Layout: {}, threshold: {:.3f},  sparsity: {}"
-          .format(layer_name, t_threshold,
-                  np.count_nonzero(t_weights[0]) / t_weights[0].size))
 
-saved_name = 'transition_model'
-
-# Train one time
-model_transition, model_transition_detail = train(model_transition,
-                                                  saved_name + '.h5', 100, 10)
-
-save_history(model_transition_detail, saved_name + '_history')
+tmp_model = load_saved_model(DEF_TRAN_PATH)
+if tmp_model is not None:
+    print('Already trained')
+    model_transition = compile_model(tmp_model)
+else:
+    model_transition = clone_model(model)
+    model_transition.set_weights(model.get_weights())
+    # Generate layer dictionary from cloned model
+    layer_dict = dict([(layer.name, layer) for layer in model_transition.layers])
+    for layer_name in prune_layouts:
+        t_layer = layer_dict[layer_name]
+        t_weights = t_layer.get_weights()
+        t_threshold = optimal_sol[layer_name]
+        t_weights[0] = prune(t_weights[0], t_threshold)
+        t_layer.set_weights(t_weights)
+        print("Layout: {}, threshold: {:.3f},  sparsity: {}"
+              .format(layer_name, t_threshold,
+                      np.count_nonzero(t_weights[0]) / t_weights[0].size))
+    # Train one time
+    model_transition, model_transition_detail = train(model_transition, 100, 10)
+    model_transition.save(DEF_TRAN_PATH)
+    save_history(model_transition_detail, DEF_TRAN_HIS_PATH)
 
 score_transition = model_transition.evaluate(x_test, y_test, verbose=0)
 print("After combine pruning, accuracy: %.2f%%" % (
         score_transition[1] * 100) + '\n')
 
 print("\n---------------Final Pruning Training -----------------------------")
-model_final = clone_model(model_transition)
-model_final.set_weights(model_transition.get_weights())
-# Generate layer dictionary from cloned model
-layer_dict = dict([(layer.name, layer) for layer in model_final.layers])
-for layer_name in prune_layouts:
-    f_layer = layer_dict[layer_name]
-    f_weights = f_layer.get_weights()
-    f_threshold = optimal_sol[layer_name]
-    f_weights[0] = prune(f_weights[0], f_threshold)
-    f_layer.set_weights(f_weights)
-    print("Layout {}, threshold: {:.3f}, sparsity: {}"
-          .format(layer_name, f_threshold,
-                  np.count_nonzero(f_weights[0]) / f_weights[0].size))
+tmp_model = load_saved_model(DEF_FINAL_PATH)
+if tmp_model is not None:
+    print('Already trained')
+    model_final = compile_model(tmp_model)
+else:
+    model_final = clone_model(model_transition)
+    model_final.set_weights(model_transition.get_weights())
+    # Generate layer dictionary from cloned model
+    layer_dict = dict([(layer.name, layer) for layer in model_final.layers])
+    for layer_name in prune_layouts:
+        f_layer = layer_dict[layer_name]
+        f_weights = f_layer.get_weights()
+        f_threshold = optimal_sol[layer_name]
+        f_weights[0] = prune(f_weights[0], f_threshold)
+        f_layer.set_weights(f_weights)
+        print("Layout {}, threshold: {:.3f}, sparsity: {}"
+              .format(layer_name, f_threshold,
+                      np.count_nonzero(f_weights[0]) / f_weights[0].size))
+    model_final = compile_model(model_final)
 
-model_final.compile(loss='sparse_categorical_crossentropy',
-                    optimizer=Adam(lr=1.0e-4),
-                    # Adam optimizer with 1.0e-4 learning rate
-                    # Metrics to be evaluated by the model
-                    metrics=['accuracy'])
 score_final = model_final.evaluate(x_test, y_test, verbose=0)
 print("After combine pruning, accuracy: %.2f%%" % (
         score_final[1] * 100) + '\n')
+model_final.save(DEF_FINAL_PATH)
+
 final_weights = model_final.get_weights()
 print("{} of {} is zero".format(np.count_nonzero(final_weights[0] == 0),
                                 final_weights[0].size))
 
-model_final.save('final_model.h5')
